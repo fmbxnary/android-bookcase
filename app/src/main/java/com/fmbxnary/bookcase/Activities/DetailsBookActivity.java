f8ad6bcc05a705e7b9e7ff4cd98ae858957d34f5
U390709004 <fatihmehmetbaydar@posta.mu.edu.tr>
2023-01-01 10:38:45 +0300

    Update DetailsBookActivity.java

--- app/src/main/java/com/fmbxnary/bookcase/Activities/DetailsBookActivity.java
+++ app/src/main/java/com/fmbxnary/bookcase/Activities/DetailsBookActivity.java
@@ -4,8 +4,14 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.viewpager2.widget.ViewPager2;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
 
 import com.fmbxnary.bookcase.Adapter.ViewPagerFragmentAdapter;
+import com.fmbxnary.bookcase.Fragments.DetailsFragment;
+import com.fmbxnary.bookcase.Fragments.NoteFragment;
+import com.fmbxnary.bookcase.Model.Book;
 import com.fmbxnary.bookcase.R;
 import com.google.android.material.tabs.TabLayout;
 import com.google.android.material.tabs.TabLayoutMediator;
@@ -25,8 +31,24 @@ public class DetailsBookActivity extends AppCompatActivity {
         tabLayout = findViewById(R.id.tab_layout);
         viewPagerFragmentAdapter = new ViewPagerFragmentAdapter(this);
 
+        // Get the Book object that you want to pass to the fragment
+        Book book = getIntent().getParcelableExtra("book");
+
+        // Create a new instance of the fragment and set the Book object as an argument
+        DetailsFragment detailsFragment = new DetailsFragment();
+        NoteFragment noteFragment = new NoteFragment();
+        Bundle data = new Bundle();
+        data.putParcelable("book2", book);
+
+        detailsFragment.setArguments(data);
+        noteFragment.setArguments(data);
+
+
+        // Add the fragment to the adapter and set it on the view pager
+        viewPagerFragmentAdapter.addFragment(detailsFragment);
+        viewPagerFragmentAdapter.addFragment(noteFragment);
         viewPager2.setAdapter(viewPagerFragmentAdapter);
 
-        new TabLayoutMediator(tabLayout, viewPager2,((tab, position) -> tab.setText(titles[position]))).attach();
+        new TabLayoutMediator(tabLayout, viewPager2, ((tab, position) -> tab.setText(titles[position]))).attach();
     }
 }
